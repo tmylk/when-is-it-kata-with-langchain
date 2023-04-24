b5217c6e1cff3dc4fee55cbd0f2bfc48bca53b3f
Lev Konstantinovskiy <lev.konst@gmail.com>
2023-04-24 18:29:51 +0200

    start state

--- test_when.py
+++ test_when.py
@@ -6,15 +6,15 @@ from pydantic import ValidationError
 from when_is_it import Answer, get_answer
 
 
-@pytest.mark.vcr(record_mode='new_episodes')
+@pytest.mark.vcr(record_mode='once')
 def test_independence_day():
     expected_answer = Answer(answer_date=date(2001, 7, 4))
     actual_answer = get_answer(question = "What date was the Independence day in USA in 2001?")
     assert expected_answer == actual_answer
 
 
-#@pytest.mark.skip(reason="Uncomment when asked in the tasklist")
-@pytest.mark.vcr(record_mode='new_episodes')
+@pytest.mark.skip(reason="Uncomment when asked in the tasklist")
+@pytest.mark.vcr(record_mode='once')
 def test_meetup_date():
     expected_answer = Answer(answer_date=date(2023, 4, 24))
     actual_answer = get_answer(question = "The meetup is on 24 April. and today is 21 April 2023. Soon it will be summer. When is the meetup?")
--- when_is_it.py
+++ when_is_it.py
@@ -30,50 +30,23 @@ def get_llm_openai():
 def get_date_llm_chain():
     llm = get_llm_cohere()
 
-    examples = [ 
-        {'question': "When is your birthday?",
-          'answer': "2020-02-24"},
-         {'question': "What is your favourite date?",
-          'answer': "1999-03-14"},
-        {'question': "What is your friend's favourite date?",
-          'answer': "1991-01-01"},
-        {'question': "When is Labour Day in 1990?",
-          'answer': "1990-05-01"},
-
-
-
-          ]
-
-    example_prompt = PromptTemplate(input_variables=["question", "answer"], template="""Question: {question} Answer: {answer}  \n""")
-
     prefix = """You are a helpful AI assisant answering only the questions that you were asked. 
     The answers are concise and to the point. If you don't know, just say "I don't know".
 
-    The correct date format is YYYY-MM-DD., e.g. 2020-02-24.
-
+    {input}
     """
 
-    date_template = FewShotPromptTemplate(
-    prefix=prefix,
-    examples=examples, 
-    example_prompt=example_prompt,
-    suffix="Question: {input} Answer:", 
-    input_variables=["input"]
+    date_template = PromptTemplate(
+        template=prefix,
+        input_variables=["input"]
     )
-    
-    
-    #new_parser = OutputFixingParser.from_llm(llm=llm, parser=None)
-    # prompt_template = PromptTemplate(template=date_template,
-    #                                 input_variables=["question"],
-    #                                 partial_variables={"format_instructions": output_parser.get_format_instructions()},
-    #                                 output_parser=output_parser)
+        
     llm_chain = LLMChain(prompt=date_template, llm=llm, )
     return llm_chain
 
 def get_a_date_str(question):
     llm_chain = get_date_llm_chain()
 
-    
     reply_str = llm_chain.run(question)
     date_str = reply_str.split("\n")[0].strip()
 
